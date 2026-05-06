d5687c439b48276f81800fbf962019feca3c4ba4
lapasovzokirjon <lapasovzokirjon4@gmail.com>
2026-05-06 01:11:38 -0700

    Meta Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import MetaPixel from "@/components/sections/MetaPixel";
+import "./globals.css";
+import Script from "next/script";
 import UTMTracker from "@/components/UTMTracker";
-import "./globals.css"; 
+
+const META_PIXEL_ID = "3807589562713636";
 
 export default function RootLayout({
   children,
@@ -10,7 +12,32 @@ export default function RootLayout({
   return (
     <html lang="uz">
       <body>
-        <MetaPixel />
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
         <UTMTracker />
         {children}
       </body>
